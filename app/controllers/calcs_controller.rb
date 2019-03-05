4bb37f55caf08f80bbf01d181904864e51732a5d
yasuda <tasssra@gmail.com>
2019-03-05 20:16:48 +0900

    CalcsControllerのshowアクションに取得のためのparams, 計算処理を追記

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,4 +1,21 @@
 class CalcsController < ApplicationController
+
   def show
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    operator = params[:operator]
+
+    case operator
+    when "addition"
+      @result = num1 + num2
+    when "subtraction"
+      @result = num1 - num2
+    when "multiplication"
+      @result = num1 * num2
+    when "division"
+      @result = num1 / num2.to_f
+    end
+
   end
+
 end
