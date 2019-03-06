814f9cebedcb8c1a6f5647902a476a4a8e487628
yasuda <tasssra@gmail.com>
2019-03-06 16:10:40 +0900

    仮ルーティングを正規のものに直した。show.html.erbに@resultを表示できるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'calcs/show' => 'calcs#show'
-  # get 'calcs/:figure1/:operator/:figure2' => 'calcs#show'
+  get 'calcs/:num1/:operator/:num2' => 'calcs#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
