49edc5f6af582244111ac0a1e04bc6850918c8ff
yasuda <tasssra@gmail.com>
2019-03-06 17:36:25 +0900

    num, operator をインスタンス変数に変更

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,19 +1,19 @@
 class CalcsController < ApplicationController
 
   def show
-    num1 = params[:num1].to_i
-    num2 = params[:num2].to_i
-    operator = params[:operator]
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @operator = params[:operator]
 
-    case operator
+    case @operator
     when "addition"
-      @result = num1 + num2
+      @result = @num1 + @num2
     when "subtraction"
-      @result = num1 - num2
+      @result = @num1 - @num2
     when "multiplication"
-      @result = num1 * num2
+      @result = @num1 * @num2
     when "division"
-      @result = num1 / num2.to_f
+      @result = @num1 / @num2.to_f
     end
 
   end
