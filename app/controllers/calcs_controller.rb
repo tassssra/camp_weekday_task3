4731e951dd8a903b175cfef0b549aa3e31f14c8a
yasuda <tasssra@gmail.com>
2019-03-07 12:04:32 +0900

    resultメソッドをCalcsHelperに移動させました

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,21 +1,13 @@
 class CalcsController < ApplicationController
+  include CalcsHelper
 
   def show
+    # http://localhost:3000/calcs/:num1/:operator/:num2
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
     @operator = params[:operator]
 
-    case @operator
-    when "addition"
-      @result = @num1 + @num2
-    when "subtraction"
-      @result = @num1 - @num2
-    when "multiplication"
-      @result = @num1 * @num2
-    when "division"
-      @result = @num1 / @num2.to_f
-    end
-
+    # CalcsHelperのresultメソッドを呼び出す
+    result
   end
-
 end
--- app/helpers/calcs_helper.rb
+++ app/helpers/calcs_helper.rb
@@ -1,2 +1,17 @@
 module CalcsHelper
+  def result
+    @result = case @operator
+              when "addition"
+                @num1 + @num2
+              when "subtraction"
+                @num1 - @num2
+              when "multiplication"
+                @num1 * @num2
+              when "division"
+                # 分母0でエラーメッセージを表示できるように三項演算子を使う
+                @num2 == 0 ? "0で割ることはできません" : @num1 / @num2.to_f # to_fメソッドで割り切れない場合に対応する
+              else
+                "計算できません。演算子を正確に入力してください。"
+              end
+  end
 end
