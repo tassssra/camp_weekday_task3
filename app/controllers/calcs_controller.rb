2358b7de9de55bf655196a2d382205c862f94a51
yasuda <tasssra@gmail.com>
2019-03-12 21:16:57 +0900

    細部の修正

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,10 +1,11 @@
 class CalcsController < ApplicationController
   include Common
-
   def show
-    # http://localhost:3000/calcs/:num1/:calc/num2
-    # Commonモジュールからcalculationメソッドを呼び出す
-    calculation
+    # http://localhost:3000/calcs/:num1/:operator/num2
+    @result = calculation(
+                params[:operator],
+                params[:num1].to_i,
+                params[:num2].to_i,
+              )
   end
-
 end
--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -1,24 +1,19 @@
 module Common
   extend ActiveSupport::Concern
-  
-  def calculation
-    num1 = params[:num1].to_i
-    num2 = params[:num2].to_i
-    operator = params[:operator]
-
-    @result = case operator
-              when "addition"
-                num1 + num2
-              when "subtraction"
-                num1 - num2
-              when "multiplication"
-                num1 * num2
-              when "division"
-                begin
-                  "#{num1 / num2}..#{num1 % num2}" # 例外が発生する可能性がある処理
-                rescue => e # 変数eに例外オブジェクトを格納
-                  e # 例外が発生した場合、変数e(例外オブジェクト)が@resultに格納される
-                end
-              end
+  def calculation(operator, num1, num2)
+    case operator
+    when "addition"
+      num1 + num2
+    when "subtraction"
+      num1 - num2
+    when "multiplication"
+      num1 * num2
+    when "division"
+      begin
+        "#{num1 / num2}..#{num1 % num2}" # 例外が発生する可能性がある処理
+      rescue => e # 変数eに例外オブジェクトを格納
+        e # 例外が発生した場合、変数eがcalculationメソッドの戻り値となって@resultに格納される
+      end
+    end
   end
 end
